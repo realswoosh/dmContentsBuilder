6242f46f50d45e78ab00393f3e65f319062ef565
Shin D M <realswoosh@gmail.com>
2017-12-25 14:01:07 +0900

    Initialize Project Add

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using dmExcelLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentsBuilder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ExcelLoader excelLoader = new ExcelLoader();
+
+			excelLoader.Load("/Data");
+		}
+	}
+}
